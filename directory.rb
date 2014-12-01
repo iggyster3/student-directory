4ea7b205663346ada9d626c8905c1a43bd3689f5
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-12-01 13:12:43 +0000

    Added Arrays to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,18 +1,29 @@
 # Declaring Student Count Variable
 student_count = 9
 
+# Aarry of students
+students = ["Dr. Hannibal Lecter",
+            "Darth Vader",
+            "Nurse Ratched",
+            "Micheal Corleone",
+            "Alex De Large",
+            "The Alien",
+            "Terminator",
+            "Freedy Kruger",
+            "The Joker"]
+
 # Print header and list of students
 puts "The students of my cohort at Makers Academy"
 puts "-------------------------------------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Micheal Corleone"
-puts "Alex De Large"
-puts "The Alien"
-puts "Terminator"
-puts "Freedy Kruger"
-puts "The Joker"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
 
 # Finally, we print the total students on the list
 puts "Overall we have #{student_count} great students"

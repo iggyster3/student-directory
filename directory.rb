9760e2e548b047c5aeb52c25e2aadd5a1083c8af
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-12-01 11:58:39 +0000

    Updated directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# Print header and list of students
 puts "The students of my cohort at Makers Academy"
 puts "-------------------------------------------"
 puts "Dr. Hannibal Lecter"
@@ -9,6 +10,9 @@ puts "The Alien"
 puts "Terminator"
 puts "Freedy Kruger"
 puts "The Joker"
+
+# Finally, we print the total
 print "Overall we have "
+# Its important that print() doesnt add a new line character
 print 9
 puts " great students."

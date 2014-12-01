1e5853be48371a3b03f55cfa97a174a54dc41b65
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-12-01 13:22:04 +0000

    Added iteration block to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,5 +1,3 @@
-# Declaring Student Count Variable
-student_count = 9
 
 # Aarry of students
 students = ["Dr. Hannibal Lecter",
@@ -15,15 +13,11 @@ students = ["Dr. Hannibal Lecter",
 # Print header and list of students
 puts "The students of my cohort at Makers Academy"
 puts "-------------------------------------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
+
+# iterate through student array and print to stdout(Console)
+students.each do |names|
+  puts names
+end
 
 # Finally, we print the total students on the list
-puts "Overall we have #{student_count} great students"
+puts "Overall we have #{students.length} great students"
